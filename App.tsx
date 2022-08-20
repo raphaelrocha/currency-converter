76ae655d74a623fc862fde5217d2ceb867577225
Raphael Rocha <raphaelrocha86@gmail.com>
2022-08-20 18:25:22 -0400

    configura react native navigation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { Box, NativeBaseProvider } from 'native-base';
 import React from 'react';
 
@@ -10,6 +11,7 @@ const App = () => {
 
   return (
     <NativeBaseProvider>
+      <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
       <Box>Hello world</Box>
     </NativeBaseProvider>
   );
